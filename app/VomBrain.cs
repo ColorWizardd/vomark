4f1aaa7eb9ca5670ede37a1bf8c4b57247b6ac09
ColorWizardd <colorwizardd.biz@gmail.com>
2025-08-14 20:30:36 -0500

    - Created base VomBrain class.

---
+++ app/VomBrain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static vomark.app.VomarkUtil;
+
+namespace vomark.app
+{
+    public class VomBrain
+    {
+        public List<VomGraph> GraphList { get; set; } = [];
+        // Include a "context" class where:
+            // Contains a label - same as the respective graph
+            // Contains an "emotion" - dictates normal/all-caps and punctuation
+        public VomGraph? FindGraph(string label)
+        {
+            return GraphList.First(x => label == x.Label) ?? null;
+        }
+    }
+}
